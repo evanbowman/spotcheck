5206bb7ebf2194cd1415adda03c0cddd8e2faef3
evanbowman <ebowman@bu.edu>
2016-10-07 19:44:22 -0400

    better documentation for core/work_queue

--- src/core/work_queue.hpp
+++ src/core/work_queue.hpp
@@ -11,10 +11,14 @@ namespace core {
 	class work_queue {
 	public:
 		explicit work_queue(const size_t pool_size);
+		// It should not need to be moved or copied.
 		work_queue(const work_queue &) = delete;
 		work_queue(work_queue &&) = delete;
 		work_queue & operator=(const work_queue &) = delete;
 		work_queue & operator=(work_queue &&) = delete;
+		// Functions submitted to a work_queue should take no
+		// arguments and have void return type. Parameters may
+		// be passed via lambda capture modes.
 		void submit(std::function<void()> && work);
 		~work_queue();
 	private:
