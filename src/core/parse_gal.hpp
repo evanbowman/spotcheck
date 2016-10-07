8370bf32cd42f8c332dba987831be031876200a5
evanbowman <ebowman@bu.edu>
2016-10-07 19:10:48 -0400

    removed static asserts

--- src/core/parse_gal.hpp
+++ src/core/parse_gal.hpp
@@ -1,15 +1,9 @@
 #pragma once
 
-#include <type_traits>
-#include <string>
-
 #include "option.hpp"
 
 namespace core {
 	struct gal_data {
-		gal_data() {
-			static_assert(!std::is_pod<gal_data>::value, "please make structs trivially constructible");
-		}
 		// Implement the structure...
 	};
 
--- src/core/parse_tiff.hpp
+++ src/core/parse_tiff.hpp
@@ -1,15 +1,9 @@
 #pragma once
 
-#include <type_traits>
-#include <string>
-
 #include "option.hpp"
 
 namespace core {
 	struct tiff_data {
-		tiff_data() {
-			static_assert(!std::is_pod<tiff_data>::value, "please make structs trivially constructible");
-		}
 		// Implement the structure...
 	};
 
