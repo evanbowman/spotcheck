629047602c2bae6c38241d1e521a35ae4e5776b7
evanbowman <ebowman@bu.edu>
2016-10-10 22:15:19 -0400

    renamed executable

--- src/core/parse_gal.cpp
+++ src/core/parse_gal.cpp
@@ -1,7 +1,7 @@
 #include "parse_gal.hpp"
 
 namespace core {
-	option<std::vector<work_unit>> parse_gal(const std::string & filename) {
+	option<std::vector<work_group>> parse_gal(const std::string & filename) {
 		// TODO:
 		// divide gal into individual units or groups of work
 		// Currently return empty object, indicating failure
