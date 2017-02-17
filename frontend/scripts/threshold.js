22aa4d74031678b8b2533c6c43bc99e5ea13469a
evanbowman <ebowman@bu.edu>
2017-02-16 23:31:52 -0500

    focus and selection boxes around threshold preview images

--- frontend/scripts/threshold.js
+++ frontend/scripts/threshold.js
@@ -26,7 +26,33 @@ function onBackPressed() {
     window.location.href = "roi.html"
 }
 
+$("#thresh-slider").on("input", function() {
+    ready(() => {
+	var tb = document.getElementById("thresh-textbox");
+	tb.value = this.value;
+    });
+});
+
 var thumbs = [];
+
+function refreshGalleryThumb(row, col) {
+    var img = thumbs[row][col];
+    var src = img.src;
+    var pos = src.indexOf("?");
+    if (pos >= 0) {
+	src = src.substr(0, pos);
+    }
+    var data = new Date();
+    img.src = src + "?v=" + data.getTime();
+}
+
+function unselAll() {
+    for (var i = 0; i < thumbs.length; ++i) {
+	for (var j = 0; j < thumbs[i].length; ++j) {
+	    thumbs[i][j].className = "gallery-box-unsel";
+	}
+    }
+}
 
 function init() {
     for (var rowNum = 0; rowNum < global.roiRows; ++rowNum) {
@@ -37,32 +63,40 @@ function init() {
 	    var img = document.createElement("img");
 	    row.push(img);
 	    img.src = pathPrefix + rowNum + colNum + ".png";
+	    img.tabIndex = 0;
 	    var div = document.createElement("div");
 	    var span = document.createElement("span");
-	    var slider = document.createElement("input");
-	    const rgbaMax = 255;
-	    slider.value = 100 * (global.backend.get_target_thresh(rowNum, colNum) / rgbaMax);
-	    slider.setAttribute("type", "range");
 	    span.appendChild(img);
-	    span.appendChild(slider);
 	    div.appendChild(span);
-	    slider.onchange = (function(tmpRow, tmpCol) {
+	    img.onclick = (function(tmpRow, tmpCol) {
 		return function() {
-		    global.backend.update_target_thresh(function() {
-			var img = thumbs[tmpRow][tmpCol];
-			var src = img.src;
-			var pos = src.indexOf("?");
-			if (pos >= 0) {
-			    src = src.substr(0, pos);
-			}
-			var date = new Date();
-			img.src = src + "?v=" + date.getTime();
-		    }, tmpRow, tmpCol, (this.value / 100) * rgbaMax);
+		    unselAll();
+		    this.className = "gallery-box-sel";
+		    document.getElementById("thresh-slider").onchange = function() {
+			global.backend.update_target_thresh(function() {
+			    refreshGalleryThumb(tmpRow, tmpCol);
+			}, tmpRow, tmpCol, this.value);
+		    }
+		    document.getElementById("thresh-textbox").onchange = function() {
+			global.backend.update_target_thresh(function() {
+			    refreshGalleryThumb(tmpRow, tmpCol);
+			    document.getElementById("thresh-slider").value = this.value;
+			}, tmpRow, tmpCol, this.value);
+		    }
+		    var currentThresh = global.backend.get_target_thresh(tmpRow, tmpCol);
+		    document.getElementById("thresh-slider").value = currentThresh;
+		    document.getElementById("thresh-textbox").value = currentThresh;
 		}
 	    })(rowNum, colNum);
-	    img.style.cssText = "width: 100px; float: left; image-rendering: crisp-edges;";
-	    $("main").append(div);
+	    img.className = "gallery-box-unsel";
+	    $("#gallery").append(div);
 	}
+	var currentThresh = global.backend.get_target_thresh(0, 0);
+	var threshSlider = document.getElementById("thresh-slider");
+	var threshTextbox = document.getElementById("thresh-textbox");
+	threshSlider.value = currentThresh;
+	threshTextbox.value = currentThresh;
+	thumbs[0][0].click();
     }
 }
 
