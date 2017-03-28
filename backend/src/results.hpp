a209cfabea6db4962e65d28cf4c4da4ca4c5ebb7
Nevin Zheng <nevzheng@gmail.com>
2017-03-28 16:15:27 -0400

    fixed results page

--- backend/src/results.hpp
+++ backend/src/results.hpp
@@ -11,9 +11,9 @@ class Result {
     int m_rowId, m_colId;
     int m_bgHeight;
     int m_area;
-    unsigned char m_min_height, m_max_height;
-    long m_volume;
-    long m_average_height;
+    double m_min_height, m_max_height;
+    double m_volume;
+    double m_average_height;
     double m_circularity;
 
 public:
